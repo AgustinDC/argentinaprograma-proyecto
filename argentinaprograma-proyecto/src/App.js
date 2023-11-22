bad9f754493528a8b5b255ff47d339d546340f66
Agustin Di Carlo <agus.dicarlo@gmail.com>
2023-11-22 10:58:23 -0300

    Se modificaron los componentes de welcome y se agrego el componente navbar que aun esta vacio

--- argentinaprograma-proyecto/src/App.js
+++ argentinaprograma-proyecto/src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Welcome from "./components/Welcome.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
     <div id="contenedor">
+      <Navbar/>
       <Welcome />
       <Welcome />
     </div>
